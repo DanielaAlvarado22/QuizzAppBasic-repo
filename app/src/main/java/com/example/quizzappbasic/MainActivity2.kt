0edd55d7895be5c9df3aa893a81a5f0ea3036cb4
ponkesito22 <71133754+DanielaAlvarado22@users.noreply.github.com>
2022-10-28 19:12:34 -0500

    Slider puesto

--- app/src/main/java/com/example/quizzappbasic/MainActivity2.kt
+++ app/src/main/java/com/example/quizzappbasic/MainActivity2.kt
@@ -4,12 +4,12 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
-import android.widget.Button
-import android.widget.CheckBox
-import android.widget.Toast
+import android.widget.*
+import org.w3c.dom.Text
 
 class MainActivity2 : AppCompatActivity() {
     lateinit var checkBox: CheckBox
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main2)
@@ -18,42 +18,40 @@ class MainActivity2 : AppCompatActivity() {
         val chk2 = findViewById<CheckBox>(R.id.checkbox2)
         val chk3 = findViewById<CheckBox>(R.id.checkbox3)
         val chk4 = findViewById<CheckBox>(R.id.checkbox4)
-        val btnVer = findViewById<Button>(R.id.btnVer)
-
-        btnVer.setOnClickListener (View.OnClickListener{
-            if (chk.isChecked){
-                Toast.makeText(this,"CHOCHA!!",Toast.LENGTH_SHORT).show()
-                chk2.isEnabled = false
-                chk3.isEnabled = false
-                chk4.isEnabled = false
-            }
-            if (chk2.isChecked){
-                Toast.makeText(this,"CULO!!",Toast.LENGTH_SHORT).show()
-                chk.isEnabled = false
-                chk3.isEnabled = false
-                chk4.isEnabled = false
-            }
-            if (chk3.isChecked){
-                Toast.makeText(this,"TETA!!",Toast.LENGTH_SHORT).show()
-                chk.isEnabled = false
-                chk2.isEnabled = false
-                chk4.isEnabled = false
-            }
-            if (chk4.isChecked){
-                Toast.makeText(this,"LA COMBI COMPLETA!!",Toast.LENGTH_SHORT).show()
-                chk.isEnabled = false
-                chk2.isEnabled = false
-                chk3.isEnabled = false
-            }
-        }
-        )
-
-
+        val chk5 = findViewById<CheckBox>(R.id.checkbox5)
+//        val btnVer = findViewById<Button>(R.id.btnVer)
+        val ExSeekBar = findViewById<SeekBar>(R.id.ExSeekBar)
+        val txtSeek = findViewById<TextView>(R.id.txtSeek)
 
         val btnRegresar = findViewById<Button>(R.id.btnRegresar)
-        btnRegresar.setOnClickListener {
-            val lanzar = Intent(this,MainActivity::class.java)
+        btnRegresar.setOnClickListener()
+        {
+            val lanzar = Intent(this, MainActivity::class.java)
             startActivity(lanzar)
         }
+
+        //slider para escoger la cantidad de preguntas
+
+        ExSeekBar.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
+            override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
+                txtSeek.text = progress.toString()
+            }
+
+            override fun onStartTrackingTouch(seekBar: SeekBar?) {
+
+            }
+
+            override fun onStopTrackingTouch(seekBar: SeekBar?) {
+
+            }
+
+        })
+
+
     }
+
+
+
+
 }
+
